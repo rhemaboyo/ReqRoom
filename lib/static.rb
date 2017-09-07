f0eafc0b405dc6ee91c1e9b75f78e7753f41f540
Rhema Boyo <rhemaboyo@gmail.com>
2017-09-06 23:08:16 -0400

    finish static class

--- lib/static.rb
+++ lib/static.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+require 'mime/types'
+
 class Static
   attr_reader :app
 
@@ -6,6 +9,21 @@ class Static
   end
 
   def call(env)
-    app.call(env)
+    req = Rack::Request.new(env)
+    path = "public/#{req.path.match(/(?<=\/public\/).+/)}"
+    response(path)
+  end
+
+  def response(path)
+    res = Rack::Response.new
+    if File.exist?(path)
+      ext = path.match(/\.\w+\.?/)[0].chomp('.')
+      res['Content-type'] = MIME::Types.type_for(ext)[0].to_s
+      res.write(File.read(path))
+    else
+      res.status = 404
+      res.write('cannot find file')
+    end
+    res.finish
   end
 end
